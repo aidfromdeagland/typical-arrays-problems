eabbdb53aa4d9542eca8df6ccb5115f19a100fc4
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-11 18:16:19 +0300

    feat: implement task functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (array && array.length) {
+      return Math.min(...array)
+    }
+    return 0
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && array.length) {
+        return Math.max(...array)
+    }
+    return 0
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && array.length) {
+        return array.reduce((acc, current) => acc += current) / array.length;
+    }
+    return 0
+};
